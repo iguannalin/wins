ff7dfdd1fb113e9f796fd7feb8483c1f9bb521ea
seasons986 <annalin12345@gmail.com>
2023-08-20 01:44:46 -0400

    aha

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
 
+  const container = document.getElementById("container");
+  let links;
+
+  function reset() {
+    container.innerHTML = "";
+    for (let i = 0; i < 12; i++) {
+      const button = document.createElement("button");
+      const img = document.createElement("img");
+      img.src = links[getRandomInt(0, links.length)];
+      button.appendChild(img);
+      button.onclick = () => {audio.play(); reset;}
+      container.appendChild(button);
+    }
+  }
+
+  let audio = new Audio("msn-sound_1.mp3");
   // all icons from -- https://win98icons.alexmeub.com/
   fetch("links.json").then((r) => r.json()).then((res) => {
-    console.log(res);
+    links = res;
+    audio.addEventListener("canplaythrough", (event) => {
+      /* the audio is now playable; play it if permissions allow */
+      reset();
+    });
   })
 });
